c8c27e6449f2d6353ff76a7359b074f5bcd9df44
Michael Dao <michael.dao@gmail.com>
2015-03-29 21:50:20 -0600

    Added API

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UrlsController < ApplicationController
   respond_to :json, :xml
 
   def show
-    respond_with Url.find_by(long: params[:id])
+    respond_with Url.find_by(long: params[:long])
   end
 end
 
